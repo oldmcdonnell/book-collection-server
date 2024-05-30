0edad41525a21fca7d2349f49d47e53149911f6a
oldmcdonnell <oldmcdonnell@gmail.com>
2024-05-30 15:44:42 -0400

    get all works

--- app_book/views.py
+++ app_book/views.py
@@ -32,12 +32,39 @@ def create_user(request):
     return Response(profile_serialized.data)
 
 
-@api_view(['GET', 'PUT'])
+@api_view(['GET', 'POST', 'PUT', 'DELETE'])
 @permission_classes([IsAuthenticated])
 def get_books(request):
-    books = Books.objects.all()
-    serialize_books = BookSerializer(books, many = True)
-    return Response(serialize_books.data)
+    if request.method == 'POST' and request.data['id']:
+        book = Books.objects.filter(id = request.data['id']).first()
+        serialize_books = BookSerializer(book)
+        print('hit ID 2 ', serialize_books.data)
+        return Response(serialize_books.data)
+    elif request.method == 'GET':
+        print('Request all book data', request)
+        book = Books.objects.all()
+        serialize_books = BookSerializer(book, many=True)
+        return Response(serialize_books.data)
+    elif request.method == 'PUT':
+        book = Books.objects.filter(id = request.data['id']).first()
+        if (request.data['title'].exists()):
+            book.title = request.data['title']
+        if (request.data['author'].exists()):
+            book.author = request.data['author']
+        if (request.data['genre'].exists):
+            book.genre = request.data['genre']
+        book.save()
+        serialize_books = BookSerializer(book)
+        return Response(serialize_books.data)
+    elif request.method == 'DELETE':
+        book = Books.objects.filter(id = request.data['id']).first()
+        serialize_books = BookSerializer(book)
+        return Response(serialize_books.data)
+    # print('REQUEST: ', request.data)
+    # books = Books.objects.all()
+    # serialize_books = BookSerializer(books, many = True)
+    # return Response(serialize_books.data)
+
 
 @api_view(['POST'])
 @permission_classes([])
